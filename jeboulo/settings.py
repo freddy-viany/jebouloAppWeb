db56b547adf5f7077063d33fc3d7a81f33223caa
freddy-viany <75251628+freddy-viany@users.noreply.github.com>
2021-03-17 08:36:49 +0100

    Update settings.py

--- jeboulo/settings.py
+++ jeboulo/settings.py
@@ -82,10 +82,7 @@ WSGI_APPLICATION = 'jeboulo.wsgi.application'
 
 DATABASES = {
 
-'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+'default': dj_database_url.config()
 
 #A supprimer ou reintegrer
 #    'default': {
