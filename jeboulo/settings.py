897fd6b66a2363ff61cabd273f39969572766f7b
freddy-viany <75251628+freddy-viany@users.noreply.github.com>
2021-03-18 20:12:33 +0100

    Update settings.py

--- jeboulo/settings.py
+++ jeboulo/settings.py
@@ -104,7 +104,7 @@ DATABASES = {
 #    }
 }
 '''
-#import dj_database_url
+import dj_database_url
 #DATABASES = { 'default': dj_database_url.config() }
 #DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 DATABASES = {'default':
