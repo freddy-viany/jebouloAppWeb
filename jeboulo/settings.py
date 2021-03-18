b190b733f1d04604ec02e3163cd3f0bce59c764b
freddy-viany <75251628+freddy-viany@users.noreply.github.com>
2021-03-18 21:49:39 +0100

    Update settings.py

--- jeboulo/settings.py
+++ jeboulo/settings.py
@@ -163,3 +163,41 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 LOCALE_PATHS = ( os.path.join(BASE_DIR, 'locale'), )
 
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format': ('%(asctime)s [%(process)d] [%(levelname)s] '
+                       'pathname=%(pathname)s lineno=%(lineno)s '
+                       'funcname=%(funcName)s %(message)s'),
+            'datefmt': '%Y-%m-%d %H:%M:%S'
+        },
+        'simple': {
+            'format': '%(levelname)s %(message)s'
+        }
+    },
+    'handlers': {
+        'null': {
+            'level': 'DEBUG',
+            'class': 'logging.NullHandler',
+        },
+        'console': {
+            'level': 'INFO',
+            'class': 'logging.StreamHandler',
+            'formatter': 'verbose'
+        }
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+            'propagate': True,
+        },
+        'django.request': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+            'propagate': False,
+        },
+    }
+}
